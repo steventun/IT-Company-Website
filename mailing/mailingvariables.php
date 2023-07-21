5173405d499f1cfebf57eec4aba6c7d18e3d65cf
steventun <kohtun.it@gmail.com>
2023-07-21 12:32:40 +0630

    Add files via upload

---
+++ mailing/mailingvariables.php
@@ -0,0 +1,7 @@
+<?php
+    $mail_host = "smtp.gmail.com";
+    $mail_port = "587";
+    $mail_sender_email = "stevenhtun.dev@gmail.com"; //sender
+    $mail_sender_password = "Kohtun!23btt"; //sender
+    $mail_sender_name = "Website Form";
+?>
